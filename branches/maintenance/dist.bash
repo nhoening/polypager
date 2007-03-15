02d496ee51d4843ae2a2394b284388438008aca5
nhoening <nhoening@3248fa2f-e11d-0410-8dff-7b227b1a3988>
2007-03-15 07:30:01 +0000

    create skins separately

--- branches/maintenance/dist.bash
+++ branches/maintenance/dist.bash
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-# makes a ZIP and a TAR.GZ out of the actual state, needs a version number
+# makes a ZIP and a TAR.GZ out of the actual state and a Zip for each skin, 
+# needs a version number
+# attempts to create a new directory in the dist directory with all the archives
 
 if [ "$1" == "" ]; then
 	echo usage: ./dist.bash {version} or bash dist.bash {version}
 else
-	#[in trunk]
+	# export from trunk
 	cd ../../trunk
 	rm -r ../export
 	svn export . ../export
 	cd ../export
-	zip -r ../PolyPager_$1.zip *
-	tar -cf ../PolyPager_$1.tar *
-	gzip -S .gz --best ../PolyPager_$1.tar 
+    mkdir ../dist/$1
+    
+    # first extract skins
+    cd style/skins
+	zip -r ../../../dist/$1/picswap.zip picswap
+    zip -r ../../../dist/$1/default.zip default
+    zip -r ../../../dist/$1/fscreen.zip fscreen
+    # keep only default
+    rm -rf picswap
+    rm -rf fscreen
+    cd ../..
+    
+    # now archive the rest
+    zip -r ../dist/$1/PolyPager_$1.zip *
+	tar -cf ../dist/$1/PolyPager_$1.tar *
+	gzip -S .gz --best ../dist/$1/PolyPager_$1.tar 
 	cd ../branches/maintenance
 fi
