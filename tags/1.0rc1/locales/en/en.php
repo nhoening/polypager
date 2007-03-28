ace37363a7f98341cea1e2ff8dc7faf1df3b5260
nhoening <nhoening@3248fa2f-e11d-0410-8dff-7b227b1a3988>
2007-03-28 12:12:49 +0000

    version 1.0rc1 ssThis line, and those below, will be ignored--
    A    tags/1.0rc1
    A    tags/1.0rc1/lib/PolyPagerLib_Sidepane.php
    A    tags/1.0rc1/lib/PolyPagerLib_Showing.php
    A    tags/1.0rc1/upgrade_db.sql

---
+++ tags/1.0rc1/locales/en/en.php
@@ -0,0 +1,78 @@
+<?
+/*
+	PolyPager - a lean, mean web publishing system
+    Copyright (C) 2006 Nicolas H�ning
+	polypager.nicolashoening.de
+	
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA' .
+*/
+
+/*This function gives german translations for english texts
+	two things need to be followed:
+	1. do HTML-Umlaute like &uuml;, also no "..." in either text
+	2. every string should be in one line (also in the original file, of course)
+	@param text: the text to be translated
+*/
+function getTranslation($text) {
+	$translation_table = array(
+/* Labels */
+//_sys_multipages / _sys_singlepages
+
+'in_menue:' => 'visible in menue:',
+'menue_index:' => 'menue-index:',
+'hide_options:' => 'hide options:',
+'hide_search:' => 'hide search:',
+'hide_toc:' => 'hide table of contents:',
+'hidden_fields:' => 'hidden fields:',
+'order_by:' => 'order by:',
+'order_order:' => 'asc/descending order of order:',
+'publish_field:' => 'publish-field:',
+'group_field:' => 'group-field:',
+'group_order:' => 'asc/descending order of groups:',
+'date_field:' => 'date-fields:',
+'edited_field:' => 'last-edited-field:',	
+'title_field:' => 'title-field:',
+'hide_comments:' => 'hide comments:',
+'hide_public_popups:' => 'hide help-popups in public area:',
+'search_month:' => 'search for month:', 	
+'search_year:' => 'search for year:',
+'search_keyword:' => 'search for keyword:',	
+'hide_labels:' => 'hide labels:',
+'search_range:' => 'search range:',
+
+//_sys_fields
+'not_brief:' => 'content is not really short:',
+'order_index:' => 'order index:',
+ 
+//_sys_sys
+'admin_name:' => 'administrator-name:',
+'admin_pass:' => 'administrator-password:',
+'feed_amount:' => 'number of feeds:',
+'full_feed:' => 'full content in feed:',
+'start_page:' => 'start-page:',
+'lang:' => 'language:',
+'submenus_always_on:' => 'submenus are always on:',
+'link_to_gallery_in_menu:' => 'show link to gallery in menu:',
+'gallery_name:'=> 'gallery name:',  	
+'gallery_index:'=> 'gallerys order index:',
+	
+//system pagenames
+'_sys_comments' => 'Comments',
+'_sys_fields' => 'Fields',
+'_sys_feed' => 'Feeds'
+);
+	return $translation_table[$text];
+}
+?>
---
+++ tags/1.0rc1/sitemap.php
@@ -0,0 +1,85 @@
+<?
+/*
+	PolyPager - a lean, mean web publishing system
+    Copyright (C) 2006 Nicolas H�ning
+	polypager.nicolashoening.de
+	
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA' .
+*/
+
+//this PHP generates thesitemap
+
+
+$path = './lib/';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+include("./lib/PolyPagerLib_Utils.php");
+
+$sys_info = getSysInfo();
+header("Content-type: text/xml; charset=".$sys_info["encoding"]);
+echo('<?xml version="1.0" encoding="'.$sys_info["encoding"].'"?>'."\n");
+
+$link = getDBLink();
+
+
+//get the path to this URI
+$doc_root_folders = explode("/", $_SERVER['DOCUMENT_ROOT']);
+$cwd__folders = explode("/", getcwd());
+//the difference between those is the path from doc root to the folder where
+//all files for this URI reside
+$path_from_doc_root = implode("/", array_diff($cwd__folders, $doc_root_folders));
+//now add to base URI
+$url = $_SERVER['HTTP_HOST'].'/'.$path_from_doc_root;
+
+
+echo('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'."\n");
+$pages = getPageNames();
+
+function prioH($pagename){
+    //simple heuristic for priority
+    global $sys_info;
+    $prio = 0.5;
+    if ($pagename == $sys_info["start_page"]) $prio = 1.0; // we want search engines to index our start page
+    if ($prio < 1 and $entity["publish_field"] != "" and $row["pub"] == '1') $prio = 0.7;
+    return $prio;
+}
+
+foreach ($pages as $p) {
+    $entity = getEntity($p);
+    if (isMultipage($p)){
+        $query = "SELECT ".$entity['pk']." AS theID, ".$entity['title_field']." AS theTitle";
+        if ($entity['publish_field']!="") $query .= ", ".$entity['publish_field']." AS pub ";
+        if ($entity['date_field']!="") $query .= ", ".$entity['date_field']["name"]." AS theDate1 ";
+        if ($entity['date_field']["editlabel"]!="") $query .= ", ".$entity['date_field']["editlabel"]." AS theDate2 ";
+        $res = pp_run_query($query."  FROM " . $entity['tablename']);
+    }else if (isSinglepage($p)){
+        $res = pp_run_query("SELECT id AS theID, heading AS theTitle, input_date AS theDate1, edited_date AS theDate2, publish AS pub FROM ".$entity['tablename']." WHERE pagename='".$p."'");
+    }
+
+    //now put'em out
+	while($row = mysql_fetch_array($res, MYSQL_ASSOC)) {
+        if ($entity["publish_field"] == "" or ($entity["publish_field"] != "" and $row["pub"] == '1')){
+            echo('	<url>'."\n");
+            echo('		<loc>http://'.$url.'?'.urlencode($p).'&amp;nr='.$row["theID"].'</loc>'."\n");
+            //prefer last edited date over input date
+            if ($row["theDate2"]!="" and $row["theDate2"]!="NULL" and substr($row['theDate2'],0,10) != '0000-00-00') echo('		<lastmod>'.date('Y-m-d',strtotime($row["theDate2"])).'</lastmod>'."\n");
+            else if ($row["theDate1"]!="" and$row["theDate1"]!="NULL" and substr($row['theDate1'],0,10) != '0000-00-00') echo('		<lastmod>'.date('Y-m-d',strtotime($row["theDate1"])).'</lastmod>'."\n");
+            echo('		<changefreq>weekly</changefreq>'."\n");
+            echo('		<priority>'.prioH($p,$entity["publish_field"],$row["pub"]).'</priority>'."\n");
+            echo('	</url>'."\n");
+        }
+    }
+}
+echo('</urlset>'."\n");
+?>
