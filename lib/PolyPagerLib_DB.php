d419775dcadae72bf6c1b16441a120d52ea25e74
Nicolas Höning <nhoening@users.noreply.github.com>
2007-11-28 23:35:52 +0000

    - make PolyPager_Config really simle - remove some echos

--- lib/PolyPagerLib_DB.php
+++ lib/PolyPagerLib_DB.php
@@ -1,6 +1,36 @@
 <?php
 
+/* this code provides the code to setup the MySQL database
+	it builds a connection (a link) to the db (once only) and returns it.
+*/
+$the_db_link = "";
+function getDBLink() {
+	global $the_db_link;
+	global $host;
+	global $db;
+	global $user;
+	global $pass;
 
+	if ($the_db_link == "") {
+        $text = "Welcome to PolyPager.<br/>Seeing this page proofs that PolyPager is working at the address you typed in. <br/> However, the database is not connectable. <br/>Maybe it is not configured yet or the configuration does not fit. <br/>If you are the administrator of this page, please consult PolyPager_Config.php";
+
+        // build connection to DBMS
+        $the_db_link = mysql_connect($host, $user, $pass) or die($text);
+        
+        // now to the DB
+        mysql_select_db($db, $the_db_link) or die ($text);
+        mysql_query('SET CHARACTER SET utf8');
+        mysql_query("SET SESSION collation_connection ='utf8_general_ci'");
+        mysql_query("SET NAMES 'utf8' COLLATE 'utf8_unicode_ci'");
+
+	}
+	return $the_db_link;
+}
+
+function getDBName() {
+	global $db;
+	return $db;
+}
 
 /*
  * run the query and append error msg to the buffer (if given) 
@@ -37,7 +67,7 @@ function isTimeType($type) {
 
 /* returns wether this MySQL type is a text type */
 function isTextType($type) {
-	return ($type == "string" or $type == "varchar" or $type == "tinytext" or
+	return ($type == "string" or $type == "varchar" or $type == "tinytext" or $type == "password" or
         $type == "set" or $type == "enum" or $type == "char" or $type == "file" or isTextAreaType($type));
 }
 
